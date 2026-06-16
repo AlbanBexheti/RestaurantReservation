bb4205e7f31d6aa9e5a3fc4a75e2010e530286c0
Alban Bexheti <122238323+AlbanBexheti@users.noreply.github.com>
2026-06-16 10:19:01 +0200

    fix: MenuItem Id nullable warning

--- backend/RestaurantReservation.API/Models/MenuItem.cs
+++ backend/RestaurantReservation.API/Models/MenuItem.cs
@@ -1,8 +1,8 @@
 namespace RestaurantReservation.API.Models
 {
-    public class MenuItem
+     public class MenuItem
     {
-        public string Id { get; set; }
+        public string Id { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
